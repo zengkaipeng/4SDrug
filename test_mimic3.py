40ea63ba2758420e8f793ae859f30c89c8613275
zengkaipeng <zkp0819@126.com>
2022-07-26 13:34:35 +0800

    testing done

--- test_mimic3.py
+++ test_mimic3.py
@@ -10,7 +10,7 @@ import time
 import dill
 
 
-def eval_one_epoch(model, data_eval, n_drug, drug_data, device):
+def eval_one_epoch(model, data_eval, n_drug, device):
     model = model.eval()
     smm_record, ja, prauc, avg_p, avg_r, avg_f1 = [[] for _ in range(6)]
     med_cnt, visit_cnt = 0, 0
@@ -19,12 +19,12 @@ def eval_one_epoch(model, data_eval, n_drug, drug_data, device):
         for adm_idx, adm in enumerate(input_seq):
             syms = torch.tensor(adm[0]).to(device)
             scores = model.evaluate(syms, device=device)
-            
+
             y_gt_tmp = np.zeros(n_drug)
             y_gt_tmp[adm[2]] = 1
             y_gt.append(y_gt_tmp)
 
-            output = torch.sigmoid(output).detach().cpu().numpy()[0]
+            output = torch.sigmoid(output).detach().cpu().numpy()
             y_pred_prob.append(output)
 
             y_pred_tmp = output.copy()
@@ -45,6 +45,8 @@ def eval_one_epoch(model, data_eval, n_drug, drug_data, device):
         avg_p.append(adm_avg_p)
         avg_r.append(adm_avg_r)
         avg_f1.append(adm_avg_f1)
+    ddi_rate = ddi_rate_score(smm_record, path='datasets/MIMIC3/ddi_A_final.pkl')
+    return np.mean(ja), np.mean(prauc), np.mean(avg_p), np.mean(avg_r), np.mean(avg_f1), 1.0 * med_cnt / visit_cnt, ddi_rate
 
 
 if __name__ == '__main__':
@@ -64,4 +66,26 @@ if __name__ == '__main__':
                   torch.tensor(pklSet.drug_multihots).to(device), args.embedding_dim).to(device)
     np.random.seed(0)
     model.load_state_dict(torch.load(args.path, map_location=device))
-
+    ddi_list, ja_list, prauc_list, f1_list, med_list = [], [], [], [], []
+    tic, result, sample_size = time.time(), [], round(len(data_test) * 0.8)
+    for _ in range(10):
+        test_sample = np.random.choice(data_test, sample_size, replace=True)
+        ja, prauc, avg_p, avg_r, avg_f1, avg_med, ddi_rate = \
+            eval_one_epoch(model, test_sample, pklSet.n_drug)
+        result.append([ddi_rate, ja, avg_f1, prauc, avg_med])
+        print('-' * 89)
+        print(
+            '| best ja {:5.4f} | prauc {:5.4f} | avg_p {:5.4f} | avg_recall {:5.4f} | '
+            'avg_f1 {:5.4f} | avg_med {:5.4f} | ddi_rate {:5.4f}'.format(ja, prauc, avg_p,
+                                                                         avg_r,
+                                                                         avg_f1, avg_med, ddi_rate))
+        print('-' * 89)
+    result = np.array(result)
+    mean, std = result.mean(axis=0), result.std(axis=0)
+    metric_list = ['ddi_rate', 'ja', 'avg_f1', 'prauc', 'med']
+    outstring = ''.join([
+        "{}:\t{:.4f} $\\pm$ {:.4f} & \n".format(metric_list[idx], m, s)
+        for idx, (m, s) in enumerate(zip(mean, std))
+    ])
+    print(outstring)
+    print('average test time: {}'.format((time.time() - tic) / 10))
